6309c38ec3c6610e7b0a1b03786b46cf940d2d78
MorshedAl <123994547+MorshedAl@users.noreply.github.com>
2023-06-04 12:59:28 +0600

    Add files via upload

---
+++ template_max.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+/*
+int maxx(int x,int y)
+{
+    if(x>y)
+        return x;
+    else
+        return y;
+
+}
+
+float maxx(float x,float y)
+{
+    if(x>y)
+        return x;
+    else
+        return y;
+}
+double  maxx(double x,double y)
+{
+    if(x>y)
+        return x;
+    else
+        return y;
+}
+
+*/
+
+/*
+//template function 
+
+//template definition
+template<class T>
+
+T maxx(T x,T y)
+{
+    if(x>y)
+        return x;
+    else
+        return y;
+}
+
+*/
+
+ template<class T>
+
+ T maxx(T x,T y)
+    {
+    
+    return x>y?x:y;
+    
+    
+    }
+
+
+
+
+
+
+
+
+
+int main()
+{
+    cout<<maxx(5,10)<<endl;
+    cout<<maxx(4.6f,8.55f)<<endl;
+    cout<<maxx(4.6,8.55)<<endl;
+
+    return 0;
+}
