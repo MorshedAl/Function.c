367943ff2f30942d605744905b7a5c1c00b13efa
MorshedAl <123994547+MorshedAl@users.noreply.github.com>
2023-06-06 10:40:07 +0600

    Add files via upload

---
+++ Call_by_reference.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+/*
+//call bu reference:
+
+ syntex is as like as call by value,
+ only difference is used '&' in formal parameter,nothing else.
+ but works as like as call by address.
+
+-in this method,swap isn’t created at stack memory.
+ just create  nick_name of variables(in stack)
+ and copy function/inline function (in main function).
+
+
+- in this method,don’t use complex anything (like loop) in swap function.
+ because here just create nick name of actual parameters only.
+so extra anything will create hassle.
+
+
+
+*/
+void swap(int &a,int &b)//formal parameters
+{
+    cout<<a<<" "<<b<<endl; // a=10,b=20
+
+    int temp=a;
+    a=b;
+    b=temp;
+    cout<<a<<" "<<b<<endl; // a=20,b=100
+}
+int main()
+{
+    int x=10,y=20;
+    swap(x,y);//actual parameters
+
+    cout<<x<<" "<<y<<endl;//x=10,y=20
+
+
+
+    return 0;
+}
